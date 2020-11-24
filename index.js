ca76c6ac84833c6a7d4b6eda03e9099f7e1baa13
md shahadat hossain <mdshahadathossain10200@gmail.com>
2020-11-23 18:05:24 -0800

    agency server side almost ready

---
+++ index.js
@@ -0,0 +1,114 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const fileupload =require('express-fileupload')
+const cors = require('cors');
+const fs =require('fs-extra')
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wawxe.mongodb.net/${process.env.DB_NAME} ?retryWrites=true&w=majority`;
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('services'));
+app.use(fileupload());
+
+
+app.get('/',(req,res)=>{
+     res.send('hello mongodb')
+})
+
+const port = 4000
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology:true });
+client.connect(err => {
+  const orderCollection= client.db("creativeAgency").collection("order");
+  const reviewCollection= client.db("creativeAgency").collection("review");
+  const serviceCollection=client.db("creativeAgency").collection("service");
+  
+
+
+  app.post('/order', (req,res) =>{
+    orderCollection.insertOne(req.body)
+    .then(result =>{
+        console.log(result);
+        res.send(result.insertedCount > 0)
+    })
+ } )
+
+ app.get('/getOrderAdmin', (req, res) => {
+    orderCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
+})
+
+
+app.post('/review-aria', (req,res) =>{
+    reviewCollection.insertOne(req.body)
+    .then(result =>{
+        res.send(result.insertedCount > 0)
+    })
+ } )
+
+ app.get('/getReview', (req, res) => {
+    reviewCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
+})
+
+app.post('/addService',(req,res)=>{
+    const file = req.files.file;
+    const title = req.body.title;
+    const Description = req.body.Description;
+    const filePath =`${__dirname}/services/${file.name}`
+
+    file.mv(filePath,err =>{
+        if (err) {
+            console.log(err);
+            res.status(4000).send({msg:'filed to upload Image '})
+
+        }
+          
+        const newImg=fs.readFileSync(filePath);
+        const encImg = newImg.toString('base64');
+        
+        
+        var image={
+            contentType:req.files.file.mimetype,
+            size:req.files.file.size,
+            img:Buffer.from(encImg,'base64')
+        };
+
+        serviceCollection.insertOne({ title, Description, image })
+          .then(result => {
+              fs.remove(filePath,error =>{
+                if (error) {
+                    console.log(error);
+                    res.status(4000).send({msg:'filed to upload Image '})
+                }
+                res.send(result.insertedCount > 0);
+              })
+          });  
+
+    })
+})
+
+app.get('/show-service', (req, res) => {
+    serviceCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
+});
+
+
+})
+
+
+
+app.listen(process.env.PORT || port);
